a9d8e271982c61e386f6e68b98c7ae237c14896b
S-JoBi23-053\student <231013@st.yoshida-g.ac.jp>
2024-06-21 10:56:35 +0900

    ペアの判定処理実装 #2 結果表示実装 #3 ループ処理実装 #4

--- ConsoleAppGE2/Card.cs
+++ ConsoleAppGE2/Card.cs
@@ -13,6 +13,13 @@ namespace ConsoleAppGE2
         public List<int> numbers { get; private set; } = new List<int>();
 
         /// <summary>
+        /// コンストラクター
+        /// </summary>
+        public Card() { 
+            numbers = new List<int>();
+        }
+
+        /// <summary>
         /// 入力処理
         /// </summary>
         /// <param name="inputNum">入力情報</param>
@@ -26,7 +33,7 @@ namespace ConsoleAppGE2
 
                 if (isSuccess && inputNum > 0 && inputNum < 5)
                 {
-                    this.numbers.Add(inputNum);
+                    numbers.Add(inputNum);
                     break;
                 }
                 else
@@ -35,6 +42,56 @@ namespace ConsoleAppGE2
                     Console.WriteLine("\n*** ERROR ***\n");
                 }
             }
+        }
+
+        /// <summary>
+        /// ペアになるカードの判定処理
+        /// </summary>
+        public void JudgePair()
+        {
+            string result = "ノーペア";
+            bool isEnd = false;
+            int i = 0;
+
+            while (!isEnd && i < numbers.Count)
+            {
+                // リストの中から同じ値の要素を探し、カウントする(自身も含まれる)
+                int pairCnt = numbers.FindAll(item => item == numbers[i]).Count();
+
+                switch (pairCnt)
+                {
+                    case 1:// １つ見つけた
+                        break;
+                    case 2:// ２つ見つけた
+
+                        // 自身と異なる値を持つ要素を探す
+                        int notPairNum = numbers.Find(item => item != numbers[i]);
+                        // notPairNumと同じ値を持つ要素があるかどうか(自身を除く)
+                        bool isHit = numbers.FindAll(item => item == notPairNum).Count() - 1 > 0;
+                        isEnd = true;
+                        if (!isHit)
+                        {
+                            result = "ワンペア";
+                        }
+                        else
+                        {
+                            result = "ツーペア";
+                        }
+                        break;
+                    case 3:// ３つ見つけた
+                        isEnd = true;
+                        result = "スリーカード";
+                        break;
+                    case 4:// ４つ見つけた
+                        isEnd = true;
+                        result = "フォーカード";
+                        break;
+                }
+
+                i++;
+            }
+
+            Console.WriteLine("結果：" + result);
         }
     }
 }
--- ConsoleAppGE2/Program.cs
+++ ConsoleAppGE2/Program.cs
@@ -8,16 +8,49 @@ namespace ConsoleAppGE2
 {
     internal class Program
     {
+        // フォームの数
         const int FormCntMax = 4;
 
         static void Main(string[] args)
         {
-            Card card = new Card();
-
-            // 1~4の値を入力するフォーム
-            for(int i = 0; i < FormCntMax; i++)
+            bool isEnd = false;
+            while (!isEnd)
             {
-                card.InputNumbers(i+1);
+                Card card = new Card();
+
+                // 1~4の値を入力するフォーム
+                for (int i = 0; i < FormCntMax; i++)
+                {
+                    card.InputNumbers(i + 1);
+                }
+
+                // ペアを探し、結果を表示する
+                card.JudgePair();
+
+                bool isSuccess = false;
+                while (!isSuccess)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\nこのあとはどうする...？ [1:もう一度あそぶ , 0:やめる]");
+                    Console.Write(">> ");
+                    isSuccess = int.TryParse(Console.ReadLine(), out int selectNum);    // 入力内容をintに変換
+
+                    // 正しく入力できた && やめる場合
+                    if (isSuccess && selectNum == 0)
+                    {
+                        isEnd = true;
+                    }
+                    // 正しく入力できていない場合
+                    else if(!isSuccess)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\n*** ERROR ***\n");
+                    }
+                    else
+                    {
+                        Console.Write("\n\n################################################\n\n\n");
+                    }
+                }
             }
 
             Console.ForegroundColor = ConsoleColor.Magenta;
