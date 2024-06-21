5c4afbe5db8cd93d050fecfca769956fd2176b4b
S-JoBi23-053\student <231013@st.yoshida-g.ac.jp>
2024-06-21 09:27:15 +0900

    プロジェクトUP＆1～4の数字を入力するフォームを4つ用意 #1

---
+++ ConsoleAppGE2/Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGE2
+{
+    internal class Card
+    {
+        // 入力情報
+        public List<int> numbers { get; private set; } = new List<int>();
+
+        /// <summary>
+        /// 入力処理
+        /// </summary>
+        /// <param name="inputNum">入力情報</param>
+        public void InputNumbers(int i)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("[{0}]１～４の数字を入力してください   > ",i);
+                bool isSuccess = int.TryParse(Console.ReadLine(), out int inputNum);
+
+                if (isSuccess && inputNum > 0 && inputNum < 5)
+                {
+                    this.numbers.Add(inputNum);
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n*** ERROR ***\n");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleAppGE2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGE2
+{
+    internal class Program
+    {
+        const int FormCntMax = 4;
+
+        static void Main(string[] args)
+        {
+            Card card = new Card();
+
+            // 1~4の値を入力するフォーム
+            for(int i = 0; i < FormCntMax; i++)
+            {
+                card.InputNumbers(i+1);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\n\n\n##### ENTERで終了 #####");
+            Console.ReadLine();
+        }
+    }
+}
